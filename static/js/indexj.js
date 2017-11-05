70852d0e80c3364e6509670a9d0f66af5e916bbb
watchdots <33148504+watchdots@users.noreply.github.com>
2017-11-05 23:28:25 +0330

    Update indexj.js

--- static/js/indexj.js
+++ static/js/indexj.js
@@ -1,4 +1,4 @@
-const socket = io('https://noters.ir:8143');
+const socket = io('https://noters.ir');
 var player, nobat, name , clicked = false;
 $('form').submit(function () {
     name = $("#nameInp").val();

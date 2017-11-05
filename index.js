44f46985c2fe4da14f572ee1dc2919fa55f1976e
watchdots <33148504+watchdots@users.noreply.github.com>
2017-11-05 23:24:20 +0330

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express'),
 
 app.use(express.static(path.join(__dirname, 'static')))
 
-http.listen(8080 , function () {
+http.listen(8143 , function () {
     console.log('listening on *:4000');
 });
 
